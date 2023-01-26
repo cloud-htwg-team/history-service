2aa66c1dbaaec54ca7589967f49c6d58e91fd1ae
Adam Radvan <radvan@inqool.cz>
2023-01-26 11:08:04 +0100

    remove IdToken from request

--- src/main/scala/de/htwg/cad/qr/history/JsonParser.scala
+++ src/main/scala/de/htwg/cad/qr/history/JsonParser.scala
@@ -4,7 +4,7 @@ import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
 
 trait JsonParser  {
-  implicit val additionRequestFormat: RootJsonFormat[CodeAdditionRequest] = jsonFormat3(CodeAdditionRequest)
+  implicit val additionRequestFormat: RootJsonFormat[CodeAdditionRequest] = jsonFormat2(CodeAdditionRequest)
   implicit val metadataShortFormat: RootJsonFormat[CodeMetadataShort] = jsonFormat3(CodeMetadataShort)
   implicit val metadataExtendedFormat: RootJsonFormat[CodeMetadataExtended] = jsonFormat4(CodeMetadataExtended)
   implicit val entryFormat: RootJsonFormat[CodeEntry] = jsonFormat5(CodeEntry)
